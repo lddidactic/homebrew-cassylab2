ce9ef4299602154bf2c338cd4e48aae92a2b8150
Karsten Koop <kkoop@ld-didactic.de>
2019-03-05 10:51:32 +0100

    Vorbereitung Umstellung auf .NET 3.5

--- Formula/cassylab2.rb
+++ Formula/cassylab2.rb
@@ -15,18 +15,36 @@ class Cassylab2 < Formula
   def install
     system "cd hidapi-lan-bridge && make && make PREFIX=#{prefix} install"
     system "wget --quiet -O cassylab2.msi http://www.ld-didactic.de/software/cassylab2_de.msi"
+    full_install = true
+    copy_from_old = false
     if Dir.exist?("#{opt_prefix}/wine_cassylab2")
-      # Update
-      system "mv #{opt_prefix}/wine_cassylab2 #{prefix}/"
-    else
-      # Neuinstallation
+      # Update nur möglich, wenn .NET-Version gleich ist
+      if `WINEPREFIX=#{opt_prefix}/wine_cassylab2 winetricks list-installed`["dotnet20"]
+        # Neuinstallation mit anschließender Kopie der Lizenzdatei
+        full_install = true
+        copy_from_old = true
+      else
+        # Update
+        system "mv #{opt_prefix}/wine_cassylab2 #{prefix}/"
+        full_install = false
+      end
+    end
+    if full_install
       system "mkdir -p #{prefix}/wine_cassylab2"
       system "WINEPREFIX=#{prefix}/wine_cassylab2 WINEARCH=win32 wineboot -i"
-      system "WINEPREFIX=#{prefix}/wine_cassylab2 WINEARCH=win32 winetricks dotnet20sp2 || true"
-      system "WINEPREFIX=#{prefix}/wine_cassylab2 WINEARCH=win32 winetricks -q dotnet20sp2"
+      system "WINEPREFIX=#{prefix}/wine_cassylab2 WINEARCH=win32 winetricks dotnet35sp1"
       system "WINEPREFIX=#{prefix}/wine_cassylab2 WINEARCH=win32 winetricks corefonts tahoma"
     end
     system "WINEPREFIX=#{prefix}/wine_cassylab2 WINEARCH=win32 wine msiexec /i cassylab2.msi /quiet"
+    if copy_from_old
+      if Dir.exist?("#{opt_prefix}/wine_cassylab2/drive_c/users/Public/Application Data/LD")
+        system "mkdir -p \"#{prefix}/wine_cassylab2/drive_c/users/Public/Application Data/\""
+        system "cp -r \"#{opt_prefix}/wine_cassylab2/drive_c/users/Public/Application Data/LD\" \"#{prefix}/wine_cassylab2/drive_c/users/Public/Application Data/\""
+      elsif Dir.exist?("#{opt_prefix}/wine_cassylab2/drive_c/ProgramData/LD")
+        system "mkdir -p \"#{prefix}/wine_cassylab2/drive_c/ProgramData/LD\""
+        system "cp -r \"#{opt_prefix}/wine_cassylab2/drive_c/ProgramData/LD\" \"#{prefix}/wine_cassylab2/drive_c/ProgramData/LD/\""
+      end
+    end
     system "cp cassylab2.sh_ cassylab2.sh"
     inreplace "cassylab2.sh","$$$PREFIX$$$","#{prefix}/wine_cassylab2"
     inreplace "cassylab2.sh","$$$LANG$$$",""
